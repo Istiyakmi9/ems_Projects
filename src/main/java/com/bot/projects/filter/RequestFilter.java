d22fd5e8266557fea8a76fa83cb93eaa436f2b13
md.istiyak <md.istiyak@primesoftinc.com>
2023-08-23 11:05:32 +0530

    removed unesd reference

--- src/main/java/com/bot/projects/filter/RequestFilter.java
+++ src/main/java/com/bot/projects/filter/RequestFilter.java
@@ -4,21 +4,14 @@ import com.bot.projects.db.utils.DatabaseConfiguration;
 import com.bot.projects.model.CurrentSession;
 import com.bot.projects.model.UserDetail;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.crypto.SecretKey;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 @Component
 public class RequestFilter implements Filter {
@@ -70,8 +63,6 @@ public class RequestFilter implements Filter {
                 databaseConfiguration.setMaxPoolSize(dbResult.getMaxPoolSize());
             }
 
-        } catch (ExpiredJwtException e) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Your session got expired");
         } catch (Exception ex) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unauthorized access. Please try with valid token.");
         }
