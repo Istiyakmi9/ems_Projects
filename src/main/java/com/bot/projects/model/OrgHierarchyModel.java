5e9496a9fc1320fdcf0910cdc53e2019d2e190df
Marghub <marghub12@gmail.com>
2024-01-02 15:37:53 +0530

    Space added

--- src/main/java/com/bot/projects/model/OrgHierarchyModel.java
+++ src/main/java/com/bot/projects/model/OrgHierarchyModel.java
@@ -12,18 +12,23 @@ public class OrgHierarchyModel {
     @Column(name = "RoleId")
     @Id
     Integer roleId;
+
     @JsonProperty("ParentNode")
     @Column(name = "ParentNode")
     Integer parentNode;
+
     @JsonProperty("RoleName")
     @Column(name = "RoleName")
     String roleName;
+
     @JsonProperty("EmployeeId")
     @Column(name = "EmployeeId")
     Long employeeId;
+
     @JsonProperty("Email")
     @Column(name = "Email")
     String email;
+
     @JsonProperty("ImageUrl")
     @Column(name = "ImageUrl")
     String imageUrl;
